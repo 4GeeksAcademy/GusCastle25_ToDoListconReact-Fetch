def03897bfa8a15b6525c3760c46ce23787384eb
GusCastle25 <157417128+GusCastle25@users.noreply.github.com>
2024-04-16 10:52:55 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from "react";
 //create your first component
-function TodoItem({ label, is_done, delete_todo, toggle_todo }){
-	return (
-		<div className="container todo-item">
-			<input type="checkbox" checked={is_done} onChange={toggle_todo}/>
-			<span className="todo-text">{label}</span>
-			<button type="button" className="btn btn-danger" onClick={delete_todo}><i className="fas fa-trash-alt"></i></button>
-		</div>
-	)
-}
 
 function Home () {
 	const [todos, setTodos] = useState([]);
 	const [todoInput, setTodoInput] = useState("");
 
 	useEffect(()=>{
+		getTodos()
+	},[])
+	console.log(todos)
+	function getTodos() {
 		fetch("https://playground.4geeks.com/todo/users/GusCastle25")
 		.then (response=> response.json())
 		.then (data => setTodos(data.todos))
 		.catch (error => console.error(error))
-	},[])
-
+	}
     // Crear usuario (POST)
     function createUser() {
         const requestOptions = {
@@ -45,13 +39,12 @@ function Home () {
         };
         fetch(`https://playground.4geeks.com/todo/todos/GusCastle25`, requestOptions)
             .then(response => response.json())
-            .then(data => { console.log(data)
-            })
+            .then(data => {setTodos(...todos, data)})
             .catch(error => console.error('Error creating user:', error));
     }
     // Función para eliminar una tarea
 	const handleDeleteTask = (todosId) => {
-   
+		console.log(todosId)
 		fetch(`https://playground.4geeks.com/todo/todos/${todosId}`, {
 			method: 'DELETE'
 		})
@@ -63,15 +56,18 @@ function Home () {
 		})
 		.catch(error => console.error('Error deleting task:', error));
 	};
-
+	function handleSubmit (e) {
+		e.preventDefault()
+		if (todoInput.length > 0) {
+			const newTask = {
+				label: todoInput,
+				is_done: false}
+			//setTodos([newTask,...todos,]); 
+			createTodo(newTask);
+			setTodoInput("");}}
+	
 	return (
-		<form onSubmit={e => {e.preventDefault();
-			if (todoInput.length > 0) {
-				const newTask = {
-					label: todoInput,
-					is_done: false}
-				setTodos([newTask,...todos,]); createTodo(newTask);
-				setTodoInput("");}}}
+		<form onSubmit={ e => handleSubmit (e)}
 			className="text-center mt-5 d-flex flex-colum align-items-center justify-content-start">
 			<div className="container">
 				<h1>Todo list</h1>
@@ -82,20 +78,21 @@ function Home () {
 					aria-label="todo list input field"
 					value={todoInput}
 					onChange={e => setTodoInput(e.target.value.trimStart())}/>
-				{todos.map((item, idx) => (<TodoItem
-					key={idx}
-					label={item.label} 
-					is_done={item.is_done} 
-					toggle_todo={() => setTodos(
-						todos.toSpliced(idx, 1 , {
-							label: item.label,
-							is_done: !item.is_done,}))}
-					delete_todo={()=> {
+				{todos?.map((item) => (
+					<div className="container todo-item" key={item.id}>
+					<input type="checkbox" checked={item.is_done} onChange={() => setTodos(
+							todos.toSpliced(item.id, 1 , {
+								label: item.label,
+								is_done: !item.is_done,}))}/>
+					<span className="todo-text">{item.label}</span>
+					<button type="button" className="btn btn-danger" onClick={()=> {
 						handleDeleteTask (item.id)
-						setTodos(todos.toSpliced(idx, 1));
-					}}/>
+						setTodos(todos.toSpliced(item.id, 1));
+					}}><i className="fas fa-trash-alt"></i></button>
+				</div>
+				
 				))}
-				<small>{todos.filter((item) => !item.is_done).length} Tasks left to do!</small>
+				<small>{todos?.filter((item) => !item.is_done).length} Tasks left to do!</small>
 			</div>
 		</form>
 	);
